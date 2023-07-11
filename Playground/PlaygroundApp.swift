9ecc9e26c910f093d34cdba10913cbe6aa003170
myhency <hency.yeo@gmail.com>
2023-07-11 22:50:13 +0900

    Initial Commit

---
+++ Playground/PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaygroundApp.swift
+//  Playground
+//
+//  Created by James on 2023/07/11.
+//
+
+import SwiftUI
+
+@main
+struct PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
